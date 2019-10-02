e5387ad3cfad18dc5e713bb54f48fa3d7e6351a4
Maksim Popov <max.popov.mail@gmail.com>
2019-10-03 00:32:19 +0300

    first commit

---
+++ static/build/precache-manifest.51310e8d265b13196e9815c0f10f15c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1ab85e532da44ba1c57356261e4fde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e35209bae7f6527611f5",
+    "url": "/static/css/2.10f3a4d0.chunk.css"
+  },
+  {
+    "revision": "bd222f32adc2e21d27d1",
+    "url": "/static/css/main.59963a4c.chunk.css"
+  },
+  {
+    "revision": "e35209bae7f6527611f5",
+    "url": "/static/js/2.4f0a7f70.chunk.js"
+  },
+  {
+    "revision": "bd222f32adc2e21d27d1",
+    "url": "/static/js/main.de7ab2f6.chunk.js"
+  },
+  {
+    "revision": "b501c6a0a1bee1e0a4f8",
+    "url": "/static/js/runtime~main.b996a010.js"
+  }
+]);
